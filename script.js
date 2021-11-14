34a53991460d57ae6f5bf8d40cdfe4001bce0337
mlecoleman <86796510+mlecoleman@users.noreply.github.com>
2021-11-14 13:41:59 -0500

    Link page for price sheet and clean up nav bar

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ calculatebutton.addEventListener ("click",(e)=>{
      
 })
 
-/*
+/* Code Ernesto helped me write in CL Monday class
 let thickness = 0;
 let width = 0;
 let length = 0;
@@ -32,7 +32,43 @@ calculatebutton.addEventListener ("click",(e)=>{
 })
 */
 
+// input
+const thicknessInput = document.querySelector('#thickness');
+const widthInput = document.querySelector('#width');
+const lengthInput = document.querySelector('#length');
+const calculateButton = document.querySelector('#calculate');
 
+calculatebutton.addEventListener ("click",(e)=>{
+    e.preventDefault ()
+})
+
+// output
+
+let output = document.getElementById('output');
+
+function boardfeetOutput() {
+
+    let thickness, width, length, boardfeet;
+
+    thickness = Number(thicknessInput.value);
+    width = Number(widthInput.value);
+    length = Number(lengthInput.value);
+
+    boardfeet = (thickness * width * length) / 144;
+
+    calculateButton.textContent = boardfeet.toFixed(2);
+
+}
+
+function calculate () {
+    if(Number(thicknessInput.value) && Number(widthInput.value) && Number(lengthInput.value)) {
+        boardfeetOutput ()
+    } else {
+        alert('Please fill in all fields with a numerical value.')
+    }
+}
+
+calculateButton.addEventListener('click', calculate);
 
 
 
