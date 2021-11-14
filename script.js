e503bc1e6238f8b46d5d7ea83ca1b1b84e6116a1
mlecoleman <86796510+mlecoleman@users.noreply.github.com>
2021-11-14 13:04:56 -0500

    Start on readme file.

--- script.js
+++ script.js
@@ -1,58 +1,40 @@
 
-/* CL Code
+let thickness = 0;
+let width = 0;
+let length = 0;
+
+let calculatebutton = document.querySelector ("#calculate");
+
+calculatebutton.addEventListener ("click",(e)=>{
+    e.preventDefault ();
+    thickness=document.querySelector ("#thickness").value;
+    width=document.querySelector ("#width").value;
+    length=document.querySelector ("#length").value;
+
+ 
+     
+})
+
+/*
 let thickness = 0;
 let width = 0;
 let length = 0;
 
 let calculatebutton = document.querySelector ("#calculate") 
 calculatebutton.addEventListener ("click",(e)=>{
-    e.preventDefault ()
-    thickness=document.querySelector ("#thickness").value
-    width=document.querySelector ("#width").value
-    length=document.querySelector ("#length").value
+    e.preventDefault ();
+    thickness=document.querySelector ("#thickness").value;
+    width=document.querySelector ("#width").value;
+    length=document.querySelector ("#length").value;
+
+ 
+     
 })
 */
 
-// input
-const thicknessInput = document.querySelector('#thickness');
-const widthInput = document.querySelector('#width');
-const lengthInput = document.querySelector('#length');
-const calculateButton = document.querySelector('#calculate');
-
-calculatebutton.addEventListener ("click",(e)=>{
-    e.preventDefault ()
-})
-
-// output
-
-let output = document.getElementById('output');
-
-function boardfeetOutput() {
-
-    let thickness, width, length, boardfeet;
-
-    thickness = Number(thicknessInput.value);
-    width = Number(widthInput.value);
-    length = Number(lengthInput.value);
-
-    boardfeet = (thickness * width * length) / 144;
-
-    calculateButton.textContent = boardfeet.toFixed(2);
-    
-}
-
-function calculate () {
-    if(Number(thicknessInput.value) && Number(widthInput.value) && Number(lengthInput.value)) {
-        boardfeetOutput ()
-    } else {
-        alert('Please fill in all fields with a numerical value.')
-    }
-}
-
-calculateButton.addEventListener('click', calculate);
 
 
-    /*document.getElementById('output').innerHTML = boardfeet;*/
+
 
 // hamburger button
 function toggleMenu () {
